87955bc4ab998328eea1bf65ce319f4f6cd2d388
Ram Lakhan Singh <62960598+lakhan5555@users.noreply.github.com>
2021-02-12 20:42:05 +0530

    Add files via upload

--- GeeksforGeeks/Greedy/1.ActivitySelection.py
+++ GeeksforGeeks/Greedy/1.ActivitySelection.py
@@ -1,1 +1,18 @@
+def ActivitySelection(a,n):
 
+    # sortef according to finish time
+    a.sort(key = lambda x: x[1])
+    s = []
+    i = 0
+    s.append(a[i])
+    for j in range(1,n):
+        if a[j][0] >= a[i][1]:
+            s.append(a[j])
+            i = j
+    return s
+
+a = [[5,9],[1,2],[3,4],[0,6],[5,7],[8,9]]
+n = len(a)
+s = ActivitySelection(a,n)
+print("Following Activites are selected: ")
+print(s)
