1ceee3f1a7d69e2346ffdb59f266dc57939df8e5
Ram Lakhan Singh <62960598+lakhan5555@users.noreply.github.com>
2021-02-13 21:37:29 +0530

    Update Q1.py

--- Must Do Ques - Geeksforgeeks/DP/Q1.py
+++ Must Do Ques - Geeksforgeeks/DP/Q1.py
@@ -1,1 +1,10 @@
-
+class Solution:
+    def minOperation(self, n):
+        if n < 1:
+            return 0
+        if n == 1:
+            return 1
+        if n % 2 == 0:
+            return self.minOperation(int(n/2)) + 1
+        else:
+            return self.minOperation(int(n/2)) + 2
